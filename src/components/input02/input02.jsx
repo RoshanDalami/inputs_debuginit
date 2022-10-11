9910196ad953a0d09eb976db4bf1ef64fffcfd59
rsunar <tech.channel.002@gmail.com>
2022-10-11 14:48:14 +0545

    placeholder animation

---
+++ src/components/input02/input02.jsx
@@ -0,0 +1,10 @@
+const Input02 = () =>{
+    return (
+        <div className="form">
+            <h2 className="header">placeHolder animation input</h2>
+            <input type="text" className="input" />
+            <label htmlFor="" className="label">Search</label>
+        </div>
+    );
+}
+export default Input02;
