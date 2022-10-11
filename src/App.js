b5b4f7688498e886f9c231fabbc6092feca09acf
rsunar <tech.channel.002@gmail.com>
2022-10-11 20:37:40 +0545

    finishing second

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import './App.css';
 import Input01 from './components/input01/Input01';
 import Input02 from './components/input02/input02';
 
+
+
 function App() {
   return (
     <div className="container">
       <div className="inner_container"><Input01/></div>
       <div className="inner_container"><Input02/></div>
-      
-      
+
+          
     </div>
   );
 }
