00c6b6c469e08eee1504de368e4a6ea97959561c
rsunar <tech.channel.002@gmail.com>
2022-10-11 18:43:41 +0545

    placeholder_input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Input01 from './components/input01/Input01';
+import Input02 from './components/input02/input02';
 
 function App() {
   return (
     <div className="container">
-      <Input01/>
+      <div className="inner_container"><Input01/></div>
+      <div className="inner_container"><Input02/></div>
+      
+      
     </div>
   );
 }
