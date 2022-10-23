ab9b745cdedb3f62e54bd19252cb449144270096
rsunar <tech.channel.002@gmail.com>
2022-10-23 11:27:19 +0530

    finishing inputs design

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-
 import './App.css';
 import Input01 from './components/input01/Input01';
 import Input02 from './components/input02/input02';
-
-
-
+import Input03 from './components/input03/input03';
 function App() {
   return (
     <div className="container">
       <div className="inner_container"><Input01/></div>
       <div className="inner_container"><Input02/></div>
+      <div className="inner_container"><Input03/></div>
 
           
     </div>
---
+++ src/components/input03/input03.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './input03.style.css'
+import {FaSearch} from 'react-icons/fa';
+function input03() {
+  return (
+    <div>
+      <h2>Expanding</h2>
+      <div className="wrapper">
+      <FaSearch className="search_icon"/>
+      <input type="text" name="" id="" className="input_input03"/>
+      </div>
+    </div>
+  );
+}
+
+export default input03;
